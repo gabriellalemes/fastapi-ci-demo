5b858f512dfa714fd61136403f2652d3dcec8cf6
gabriellalemes <gabriella.lemes@aluno.impacta.edu.br>
2026-04-12 00:09:56 -0300

    adiciona teste da multiplicacao corrigido

--- test_main.py
+++ test_main.py
@@ -6,3 +6,7 @@ client = TestClient(app)
 def test_somar():
     response = client.get("/somar/10/5")
     assert response.json() == {"resultado": 15}
+
+def test_multiplicar():
+    response = client.get("/multiplicar/2/2")
+    assert response.json() == {"resultado": 4}
